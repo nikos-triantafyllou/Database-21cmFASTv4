bc00a90676f5ee208dc0424cdadd5791d5d89244
Nikolaos Triantafyllou <ntriant1@login05.leonardo.local>
2024-10-21 12:46:09 +0200

    plotting

---
+++ plots/plot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+#SBATCH --nodes=1                    # nodes
+#SBATCH --ntasks-per-node=1          # tasks/processes/cores per node max 112 for DCGP and 32 for Booster
+#SBATCH --cpus-per-task=1            # threads/cpus per task
+#SBATCH --mem=1GB                   # memory per simulation
+#SBATCH --time=12:00:00              # time limits: 12 hours
+#SBATCH --error=myJob.err            # standard error file 
+#SBATCH --account=CNHPC_1497299_0    # account name (CNHPC_1497299_0 or CNHPC_1497299 for dcgp and booster respectively)
+#SBATCH --output=myJob.out           # standard output file``
+#SBATCH --partition=dcgp_usr_prod    # partition name (<dcgp or boost>_usr_prod)
+#SBATCH --qos=normal                 # quality of service
+
+#SBATCH --array=1-1                 # number of simulations 
+
+source ~/.bashrc
+module load python/3.10
+module load gcc/12.2.0
+module load openblas/0.3.24--gcc--12.2.0
+module load gsl/2.7.1--gcc--12.2.0
+module load openmpi/4.1.6--gcc--12.2.0
+module load fftw/3.3.10--openmpi--4.1.6--gcc--12.2.0
+
+
+source /leonardo_work/CNHPC_1497299/ntriantafyllou/database/database3_venv/venv3/bin/activate  
+
+
+
+COUNTER_PATH="/leonardo_work/CNHPC_1497299/ntriantafyllou/database/database3_venv/CREATE_DATABASE/counter.txt"
+COUNTER=$(cat $COUNTER_PATH)
+
+
+# ARGS=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+ARGS=({0..20})
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK 
+
+
+srun /leonardo_work/CNHPC_1497299/ntriantafyllou/database/database3_venv/venv3/bin/python /leonardo_work/CNHPC_1497299/ntriantafyllou/database/database3_venv/CREATE_DATABASE/plots/plot_corner.py \
+--counter ${ARGS[$SLURM_ARRAY_TASK_ID-1]} \
+--threads $SLURM_CPUS_PER_TASK &
+
+
+wait
